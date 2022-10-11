6127dba890d72745ed5db3a37edd3770028fa94a
Duzasowa <harencko.vitaliy@gmail.com>
2022-10-11 21:44:59 +0200

    add redux in single product

--- client/src/Redux/Actions/ProductActions.js
+++ client/src/Redux/Actions/ProductActions.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../Constants/ProductConstants"
+import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../Constants/ProductConstants"
 
+
+// PRODUCT LIST
 export const listProduct = () => async(dispatch) => {
   try {
     dispatch({type:PRODUCT_LIST_REQUEST})
@@ -11,8 +13,25 @@ export const listProduct = () => async(dispatch) => {
       type: PRODUCT_LIST_FAIL,
       payload:
         error.respons && error.response.data.message 
-        ? error.response.data.message 
-        : error.message,
+          ? error.response.data.message 
+          : error.message,
+    });
+  }
+};
+
+// SINGLE PRODUCT
+export const listProductDetails = (id) => async(dispatch) => {
+  try {
+    dispatch({type:PRODUCT_DETAILS_REQUEST})
+    const {data} = await axios.get(`/api/products/${id}`);
+    dispatch({type:PRODUCT_DETAILS_SUCCESS,payload:data})
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.respons && error.response.data.message 
+          ? error.response.data.message 
+          : error.message,
     });
   }
 };
--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReducer } from "./Reducer/ProductReducer";
+import { productDetailsReducer, productListReducer } from "./Reducer/ProductReducer";
 
 const reducer = combineReducers({
-  productList : productListReducer
+  productList : productListReducer,
+  productDetails : productDetailsReducer
 });
 
 const initialState = {}
