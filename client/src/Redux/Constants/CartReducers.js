a16d177940a4bf1e39f0fd2c87a73cdbaa430a0f
Duzasowa <harencko.vitaliy@gmail.com>
2022-10-13 20:45:49 +0200

    cart redux

---
+++ client/src/Redux/Constants/CartReducers.js
@@ -0,0 +1,28 @@
+import { CART_ADD_ITEM } from "./CartConstants"
+
+export const cartReducer = (state = {cartItem: [] },action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x.product === item.product);
+
+      if (existItem) {
+          return {
+            ...state,
+            cartItem: state.cartItem.map((x) => 
+              x.product === existItem.product ? item : x 
+            ),
+          };
+      } else {
+          return {
+            ...state,
+            cartItems: state.cartItems.filter(
+              (x) => x.product !== action.payload
+            ),
+          };
+      }
+
+    default:
+      return state;
+  }
+}
