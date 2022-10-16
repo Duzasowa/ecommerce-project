7ca862b02ddb752936b53b19f3d9d96cdc215b1f
Duzasowa <harencko.vitaliy@gmail.com>
2022-10-16 16:23:09 +0200

    login redux

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -3,21 +3,31 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productDetailsReducer, productListReducer } from "./Reducers/ProductReducer";
 import { cartReducer } from "./Reducers/CartReducers";
+import { userLoginReducer } from "./Reducers/UserReducers";
 
 const reducer = combineReducers({
   productList : productListReducer,
   productDetails : productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
-  : []
+  : [];
+
+//LOGIN
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 const initialState = {
   cart: {
-    cartItems: cartItemsFromLocalStorage
-  }
+    cartItems: cartItemsFromLocalStorage,
+  },
+  userLogin: {
+    userInfo: userInfoFromLocalStorage
+  },
 };
 
 const middleware = [thunk];
