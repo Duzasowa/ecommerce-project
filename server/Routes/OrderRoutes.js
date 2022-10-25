75d10f742dacc25048fcc370fabff9c8c73aef9e
Duzasowa <harencko.vitaliy@gmail.com>
2022-10-25 23:59:55 +0200

    order screen

--- server/Routes/OrderRoutes.js
+++ server/Routes/OrderRoutes.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express"; 
 import asyncHandler from "express-async-handler";
 import protect from "../Middleware/AuthMiddleware.js";
 import Order from "./../Models/OrderModel.js";
@@ -42,15 +42,6 @@ orderRouter.post(
   })
 );
 
-// USER LOGIN ORDERS
-orderRouter.get(
-  "/",
-  protect,
-  asyncHandler(async (req, res) => {
-    const order = await Order.find({ user: req.user._id }).sort({ _id: -1 });
-    res.json(order);
-  })
-);
 
 // GET ORDER BY ID
 orderRouter.get(
@@ -71,30 +62,5 @@ orderRouter.get(
   })
 );
 
-// ORDER IS PAID
-orderRouter.put(
-  "/:id/pay",
-  protect,
-  asyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id);
-
-    if (order) {
-      order.isPaid = true;
-      order.paidAt = Date.now();
-      order.paymentResult = {
-        id: req.body.id,
-        status: req.body.status,
-        update_time: req.body.update_time,
-        email_address: req.body.email_address,
-      };
-
-      const updatedOrder = await order.save();
-      res.json(updatedOrder);
-    } else {
-      res.status(404);
-      throw new Error("Order Not Found");
-    }
-  })
-);
 
 export default orderRouter;
