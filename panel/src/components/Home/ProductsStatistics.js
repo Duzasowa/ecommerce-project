5fdc61425e0552e94e41168c26ccf19d467e65ea
Duzasowa <harencko.vitaliy@gmail.com>
2022-11-13 21:05:33 +0100

    STATISTIC & DASHBOARD

--- panel/src/components/Home/ProductsStatistics.js
+++ panel/src/components/Home/ProductsStatistics.js
@@ -6,10 +6,16 @@ const ProductsStatistics = () => {
       <div className="card mb-4 shadow-sm">
         <article className="card-body">
           <h5 className="card-title">Products statistics</h5>
-          <img
-            style={{ width: "100%", height: "350px", objectFit: "contain" }}
-            src="/images/product.png"
-          />
+          <iframe 
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-ecommerce-project-rrwkn/embed/charts?id=637141d1-e681-4be7-8ce5-94288a688b6c&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
         </article>
       </div>
     </div>
--- panel/src/components/Home/SalesStatistics.js
+++ panel/src/components/Home/SalesStatistics.js
@@ -6,10 +6,16 @@ const SaleStatistics = () => {
       <div className="card mb-4 shadow-sm">
         <article className="card-body">
           <h5 className="card-title">Sale statistics</h5>
-          <img
-            style={{ width: "100%", height: "350px", objectFit: "contain" }}
-            src="/images/static.png"
-          />
+          <iframe 
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-ecommerce-project-rrwkn/embed/charts?id=63713a24-01a5-4734-815b-69d5cdb961d5&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
         </article>
       </div>
     </div>
